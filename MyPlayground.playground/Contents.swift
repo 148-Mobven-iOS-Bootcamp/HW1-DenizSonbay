fded500dd3d50ac7b010d387d88d714480ce4023
Saim Deniz Sonbay <sdsonbay@gmail.com>
2021-12-17 23:03:15 +0300

    explanations added

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,16 @@
 import UIKit
-
+//Definition of required variables
 var number1 = 0
 var number2 = 1
 var nextNumber = 1
-
+//Loop for increase the numbers in the sequence
 while nextNumber <= 1000{
     nextNumber = number1 + number2
+//Conditional statement for printing the intended values
     if (nextNumber >= 750) && (nextNumber <= 1000){
         print(nextNumber)
     }
+//Required equations for shifting numbers
     number1 = number2
     number2 = nextNumber
 }
